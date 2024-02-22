57d84983a79d4867fcba887c6d731e9682bb968b
diaszakir <153705027+diaszakir@users.noreply.github.com>
2024-02-22 22:12:15 +0600

    Wolf.java

--- src/Wolf.java
+++ src/Wolf.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Wolf {
+public class Wolf implements Enemy{
+
+    @Override
+    public void information() {
+        System.out.println("This is wolf. It has 150 health points and 20 damage");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Wolf noticed the hero!");
+    }
+
+    @Override
+    public void attack() {
+        System.out.println("Wolf attacks");
+    }
 }
