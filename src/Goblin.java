23061f6a9c387d457781c1b725d212d59865b3d1
diaszakir <153705027+diaszakir@users.noreply.github.com>
2024-02-22 22:10:43 +0600

    Goblin.java

--- src/Goblin.java
+++ src/Goblin.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Goblin {
+public class Goblin implements Enemy{
+
+    @Override
+    public void information() {
+        System.out.println("This is goblin. It has 100 health points and 10 damage");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Goblin noticed the hero!");
+    }
+
+    @Override
+    public void attack() {
+        System.out.println("Goblin attacks");
+    }
 }
